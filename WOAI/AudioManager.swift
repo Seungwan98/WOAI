9be2ce13a4b61b37dc957cc5d50f9a95c82f9975
Seungwan98 <sinna3210@gmail.com>
2024-12-04 21:45:23 +0900

    add: UI 및 구조 변경

---
+++ WOAI/AudioManager.swift
@@ -0,0 +1,16 @@
+//
+//  AudioManager.swift
+//  WOAI
+//
+//  Created by 양승완 on 12/4/24.
+//
+
+import AVFAudio
+import AVFoundation
+import Foundation
+final class AudioManager {
+    
+    static let shared = AudioManager()
+    
+ 
+}
--- WOAI/ViewControllers/MainVC.swift
+++ WOAI/ViewControllers/MainVC.swift
@@ -10,34 +10,58 @@ import OpenAI
 import WhisperKit
 import RxSwift
 import AVFAudio
+import SnapKit
+import Then
+
 class mainVC: UIViewController, AVAudioRecorderDelegate {
     private var audioRecorder: AVAudioRecorder?
     private let audioSession = AVAudioSession.sharedInstance()
     private var recordingFileURL: URL?
+    
+    
+    private var recordBtn = UIButton().then {
+        $0.setTitle("Record", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.backgroundColor = .red
+    }
+    
+    @objc
     func record() {
-        let fileName = "testing\(UUID()).wav"
-        let fileURL = FileManager.default.urls(for: .documentDirectory , in: .userDomainMask).first!.appendingPathComponent(fileName)
-                recordingFileURL = fileURL
-                
-                let settings: [String: Any] = [
-                    AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-                    AVSampleRateKey: 44100,
-                    AVNumberOfChannelsKey: 1,
-                    AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-                ]
-                
-                do {
-                    audioRecorder = try AVAudioRecorder(url: fileURL, settings: settings)
-                    audioRecorder?.delegate = self
-                    audioRecorder?.record()
-                    print("Recording started at \(fileURL)")
-                } catch {
-                    print("Failed to start recording: \(error)")
-                }
+        
+        if let recorder = audioRecorder, recorder.isRecording {
+            audioRecorder?.stop()
+
+        } else {
+            print("stop")
+            self.recordBtn.setTitle("stop", for: .normal)
+
+            let fileName = "testing\(UUID()).wav"
+            let fileURL = FileManager.default.urls(for: .documentDirectory , in: .userDomainMask).first!.appendingPathComponent(fileName)
+            recordingFileURL = fileURL
+            
+            let settings: [String: Any] = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 44100,
+                AVNumberOfChannelsKey: 1,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+            
+            do {
+                audioRecorder = try AVAudioRecorder(url: fileURL, settings: settings)
+                audioRecorder?.delegate = self
+                audioRecorder?.record()
+                print("Recording started at \(fileURL)")
+            } catch {
+                print("Failed to start recording: \(error)")
+            }
+            
+        }
+     
     }
     
     func stop() {
         print("stop")
+        self.recordBtn.setTitle("record", for: .normal)
         audioRecorder?.stop()
     }
 
@@ -47,6 +71,21 @@ var disposeBag = DisposeBag()
     let tempDir = FileManager.default.temporaryDirectory
     let mainVm = MainVM()
     
+    func setUI() {
+        
+        self.view.addSubview(recordBtn)
+        
+        recordBtn.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.height.equalTo(100)
+        }
+    }
+    
+    func setBtns() {
+        
+        recordBtn.addTarget(self , action: #selector(record), for: .touchUpInside)
+    }
+    
     
     
     override func viewDidLoad() {
@@ -55,20 +94,26 @@ var disposeBag = DisposeBag()
     
 
         // 권한
-        AVAudioSession.sharedInstance().requestRecordPermission { (accepted) in
-            if accepted {
-                print("permission granted")
+        AVAudioApplication.requestRecordPermission(completionHandler: { com in
+            if com {
+                
+                print("권한 설정")
+                
+            } else {
+                
+                print("권한 해제" )
+                
             }
-        }
+            
+            
+        })
 
         setBindings()
-        record()
+        setUI()
+        setBtns()
         
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 10, execute: {
-            
-            self.stop()
-        } )
+       
 //        Task {
 //           let pipe = try? await WhisperKit()
 //            
